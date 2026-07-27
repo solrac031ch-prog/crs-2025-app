12b0823490a8f02358d5b3f0b710985e59cd5f9b
solrac031ch-prog <solrac031.ch@gmail.com>
2026-07-27 14:19:07 -0400

    Retirar archivo aislado de preparación

--- gestion-pacientes-config.js
+++
@@ -1,3 +0,0 @@
-window.CRS_PATIENT_CASES_CONFIG = Object.freeze({
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxfahyEJq8CYnyUNG-PKKT8h0XmpTMILeSLziAxweFKmUwd11xNUFx-G_ACPg19b1Ox/exec"
-});
