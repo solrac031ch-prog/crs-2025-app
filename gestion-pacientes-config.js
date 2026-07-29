6e2d59af4267311b0b61a49b7ff053679c261d75
solrac031ch-prog <solrac031.ch@gmail.com>
2026-07-29 13:45:06 -0400

    Apuntar gestión ambulatoria al despliegue Apps Script v4

--- gestion-pacientes-config.js
+++ gestion-pacientes-config.js
@@ -1,3 +1,3 @@
 window.CRS_PATIENT_CASES_CONFIG = Object.freeze({
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwWEslkO5R42-rUCtFM8K49DQ1UPve5_U3D5XBU4Eb5491puNIeM1l2JcuycZfX6MXy/exec"
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbyoWv7y_zH3V29SrOp3UbjHEZnX4Li65CSk0hO_n2Yu5dCyLZ0CAiN-F7UIHwz2aIUJ/exec"
 });
