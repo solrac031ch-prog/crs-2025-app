f5f630bb6ec845b1dd9d6f3426bcfd717e5b5083
solrac031ch-prog <solrac031.ch@gmail.com>
2026-09-07 05:13:24 -0300

    feat: cargar backfill de rotativa estructurada

--- protocolos-detalle-polish.js
+++ protocolos-detalle-polish.js
@@ -21,6 +21,7 @@
 
   addStyle('./master-upgrade.css?v=1', 'style');
   addScript('./llamados-estructurados.js?v=2', 'calls-structured');
+  addScript('./llamados-backfill.js?v=1', 'calls-backfill');
   addScript('./master-source-audit.js?v=1', 'source-audit');
   addScript('./master-search.js?v=1', 'search');
 
