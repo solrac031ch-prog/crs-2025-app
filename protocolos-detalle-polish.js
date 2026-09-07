87e6f5a1a930b4459fc924b1b5c01ac26232697f
solrac031ch-prog <solrac031.ch@gmail.com>
2026-09-07 06:25:04 -0300

    chore: cargar compatibilidad de llamados estructurados

--- protocolos-detalle-polish.js
+++ protocolos-detalle-polish.js
@@ -21,6 +21,7 @@
 
   addStyle('./master-upgrade.css?v=1', 'style');
   addScript('./llamados-estructurados.js?v=3', 'calls-structured');
+  addScript('./llamados-estructurados-compat.js?v=1', 'calls-structured-compat');
   addScript('./llamados-backfill.js?v=2', 'calls-backfill');
   addScript('./master-source-audit.js?v=1', 'source-audit');
   addScript('./master-search.js?v=1', 'search');
